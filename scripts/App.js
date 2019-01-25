be00c2a7c60d0180d164a20f757c9ac2fc8f7128
Joey Gomez-Benito <jgbenito7@gmail.com>
2019-01-25 15:15:24 -0800

    feat: add grid

--- scripts/App.js
+++ scripts/App.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { css } from "emotion";
+
+const styles = {
+  board: css`
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    width: 500px;
+    height: 500px;
+    background: magenta;
+  `,
+
+  square: css``
+};
 
 export default class App extends Component {
   render() {
     return (
-      // Add your component markup and other subcomponent references here.
-      <h1>Hello, World!</h1>
+      <div className={styles.board}>
+        <button />
+        <button />
+        <button />
+        <button />
+        <button />
+        <button />
+        <button />
+        <button />
+        <button />
+      </div>
     );
   }
 }
